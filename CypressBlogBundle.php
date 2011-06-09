79530455d01f59224d9813fbabb632f790246dcb
matteosister <matteog@gmail.com>
2011-06-10 01:34:27 +0200

    boot deleted from bundle root class

--- CypressBlogBundle.php
+++ CypressBlogBundle.php
@@ -3,16 +3,7 @@
 namespace Cypress\BlogBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class CypressBlogBundle extends Bundle
 {
-    /**
-     * Boots the Bundle.
-     */
-    public function boot()
-    {
-        parent::boot();
-    }
 }
