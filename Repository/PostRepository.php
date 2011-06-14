caa6968d5607d28864f6f8ea1488c9f99f8565e7
matteosister <matteog@gmail.com>
2011-06-14 21:28:03 +0200

    blog

---
+++ Repository/PostRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * @matteosister
+ * https://github.com/matteosister
+ * Just for fun...
+ */
+
+namespace Cypress\BlogBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class PostRepository extends EntityRepository {
+    
+}
